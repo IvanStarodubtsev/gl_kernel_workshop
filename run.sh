b89747c52ea232aac0b3f7f23c53a86ab7ae1f22
Ivan Starodubtsev <ivan@ezlo.com>
2016-11-21 19:59:45 +0200

    Oh finally we eliminated absence of prompt

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/bin/bash -evx
 
-qemu-system-i386	-nographic 								\
-			-boot c 								\
-			-kernel buildroot/output/images/bzImage					\
-			-drive 	file=buildroot/output/images/rootfs.ext2,format=raw,media=disk 	\
-			-netdev user,id=vmnic -device virtio-net,netdev=vmnic			\
-			-append "root=/dev/sda rw console=ttyS0 init=/sbin/init"
+BR_KERNEL=buildroot/output/images/bzImage
+BR_ROOTFS=buildroot/output/images/rootfs.ext2
+
+qemu-system-i386	-nographic						\
+			-M pc							\
+			-boot c							\
+		 	-kernel $BR_KERNEL					\
+		 	-drive file=$BR_ROOTFS,if=virtio,format=raw 		\
+		 	-append "root=/dev/vda rw console=ttyS0,115200"		\
+		 	-net nic,model=virtio -net user
 
