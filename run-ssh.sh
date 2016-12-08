974c08154e81d840e615ae909786aac1740993c1
Ivan Starodubtsev <ivan@ezlo.com>
2016-12-08 17:03:41 +0200

    run ssh changed

--- run-ssh.sh
+++ run-ssh.sh
@@ -1,8 +1,11 @@
-#!/bin/bash -ex
+#!/bin/bash
 
-[ -z $1 ] && exit
+[ -z $1 ] && echo -e "\n\nusage: $0 (user|root)\n\n" && exit
 
 SSH_USER=$1
+
+# let's do this everytime we are trying to connect
+ssh-keygen -f "/home/ivan/.ssh/known_hosts" -R [localhost]:8022
 
 sshpass -p "pass" ssh -o "StrictHostKeyChecking=no" -p 8022 ${SSH_USER}@localhost
 
